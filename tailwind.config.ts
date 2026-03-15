292e462426b194d978e0ff3808ea718ce8d7b4c8
gyn <gyn38k@gmail.com>
2026-03-15 19:18:54 +0900

    cover 애니메이션

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {// 1. 애니메이션 동작(움직임) 정의
