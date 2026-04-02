f725380899d86dc4cdea9b0757aa03988723d6c8
김가연 <gyn38k@gmail.com>
2026-04-02 10:03:42 +0900

    버그수정

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,8 +1,12 @@
 // lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-// Vercel 환경 변수에서 값을 가져오고, 없으면 빈 문자열을 기본값으로 설정
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env
+  .NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY as string;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("환경 변수가 설정되지 않았습니다!");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
